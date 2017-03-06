4a0f68b9fd6361d145b97a6887ea53baa9c43531
iam91 <zhengwy91@foxmail.com>
2017-03-06 22:28:30 +0800

    update

--- src/zmarkdown.js
+++ src/zmarkdown.js
@@ -6,13 +6,13 @@
         blank_line: /^ *$/,
         list_item_width: /^([-_*]|\d{1,9}\.)( +)(\S+|$)/,
 
-        block_quote: /^> /,
-        list: /^([-_*]|\d{1,9}\.) /,
-        u_list: /^[-_*] /,
-        o_list: /^\d{1,9}\. /,
+        block_quote: /^ {0,3}> /,
+        list: /^ {0,3}([-_*]|\d{1,9}\.) /,
+        u_list: /^ {0,3}[-_*] /,
+        o_list: /^ {0,3}\d{1,9}\. /,
 
-        thematic: /^([*-_]) *\1 *\1 *(\1 *)*$/,
-        atx: /^#{1,6}($| +)/,
+        thematic: /^ {0,3}([*-_]) *\1 *\1 *(\1 *)*$/,
+        atx: /^ {0,3}#{1,6}($| +)/,
         indented_code: /^ {4,}/
     };
 
@@ -48,8 +48,15 @@
          * @returns {Boolean}
          */
         transverse: function(node, line){
-            var indentWidth = Util.indentWidth(line);
-            line = Util.consumeExtraIndent(line);
+            if(node.type !== 'doc'
+                && node.type !== 'u_list'
+                && node.type !== 'o_list'
+                && node.type !== 'u_list_item'
+                && node.type !== 'o_list_item'){
+                var indentWidth = Util.indentWidth(line);
+                line = Util.consumeExtraIndent(line, indentWidth);
+            }
+
             if(node instanceof ContainerNode){
                 if(node.open && node.isContent(line)){
                     var n = node.children.length;
@@ -59,7 +66,7 @@
                         if(r){ return true; }
                     }
                     var contentType = Node.contentType(node, rest);
-                    var newNode = Node.factory(contentType, rest, indentWidth);
+                    var newNode = Node.factory(contentType, rest);
                     if(contentType.container){
                         Node.transverse(newNode, rest);
                     }
@@ -104,21 +111,22 @@
             }
         },
 
-        factory: function(contentType, line, indentWidth){
-            var node = null;
+        factory: function(contentType, line){
             if(contentType.container){
                 if(contentType.type === 'block_quote'){
                     return new ContainerNode('block_quote', null);
                 }else if(contentType.type === 'u_list'){
                     var markerWidth = Util.listMarkerWidth(line);
-                    return new ContainerNode('u_list', {markerWidth: markerWidth + indentWidth});
+                    return new ContainerNode('u_list', {markerWidth: markerWidth});
                 }else if(contentType.type === 'o_list'){
                     var markerWidth = Util.listMarkerWidth(line);
-                    return new ContainerNode('o_list', {markerWidth: markerWidth + indentWidth});
+                    return new ContainerNode('o_list', {markerWidth: markerWidth});
                 }else if(contentType.type === 'u_list_item'){
-                    return new ContainerNode('u_list_item', null);
+                    var markerWidth = Util.listMarkerWidth(line);
+                    return new ContainerNode('u_list_item', {markerWidth: markerWidth});
                 }else if(contentType.type === 'o_list_item'){
-                    return new ContainerNode('o_list_item', null);
+                    var markerWidth = Util.listMarkerWidth(line);
+                    return new ContainerNode('o_list_item', {markerWidth: markerWidth});
                 }
             }else{
                 if(contentType.type === 'thematic'){
@@ -148,7 +156,25 @@
     }
 
     ContainerNode.prototype.isContent = function(line){
-        return ContainerNode.helpers.isContent[this.type](line);
+        if(this.type === 'u_list'
+            || this.type === 'o_list'
+            || this.type === 'u_list_item'
+            || this.type === 'o_list_item'){
+
+            var markerWidth = this.misc.markerWidth;
+            var cap = /^( *)\S+/.exec(line);
+            var indent = cap[1].length;
+
+            var matchMarker = Marker.list.test(line);
+
+            if(this.type === 'u_list_item' || this.type === 'o_list_item'){
+                return !matchMarker && indent >= markerWidth;
+            }else if(this.type === 'u_list' || this.type === 'o_list'){
+                return matchMarker || indent >= markerWidth;
+            }
+        }else{
+            return ContainerNode.helpers.isContent[this.type](line);
+        }
     };
 
     ContainerNode.prototype.rest = function(line){
@@ -166,9 +192,7 @@
             'doc': function(){ return true; },
             'block_quote': function(line){
                 return !Util.isBlankLine(line) && Marker.block_quote.test(line);
-            },
-            'u_list': function(line){ return Marker.u_list.test(line); },
-            'o_list': function(line){ return Marker.o_list.test(line); }
+            }
         },
         rest: {
             'doc': function(line){ return line; },
@@ -176,7 +200,9 @@
                 return line.substring(2);
             },
             'u_list': function(line){ return line; },
-            'o_list': function(line){ return line; }
+            'o_list': function(line){ return line; },
+            'u_list_item': function(line){ return line.substring(2); },
+            'o_list_item': function(line){ return line; }
         }
     };
 
