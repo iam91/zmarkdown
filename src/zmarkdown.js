f7df91616b55c3ae4673b74a81f34866d3df4ff5
iam91 <zhengwy91@foxmail.com>
2017-03-08 15:16:29 +0800

    update

--- src/zmarkdown.js
+++ src/zmarkdown.js
@@ -1,18 +1,20 @@
 /**
  * Created by zwy on 17-3-7.
  */
-(function(global){
+;(function(global){
     "use strict";
 
-    // todo 不同列表`marker`需要建立新的列表
-    // todo 有序列表，重构列表的添加
-    // todo `blank line`不会截断`indented code`
-    // todo `blank line`造成`loose list`
+    /* todo `blank line`不会截断`indented code` */
+    /* todo `blank line`造成`loose list` */
+
+    /**
+     * Parse markdown to block tree.
+     */
 
     var Marker = {
         blank_line: /^\s*$/,
 
-        thematic: /^ {0,3}([*-_]) *\1 *\1 *(\1 *)*$/,
+        thematic: /^ {0,3}([*-_]){3}\s*$/,
         atx: /^ {0,3}#{1,6}($| +)/,
         indented_code: /^ {4,}/,
 
@@ -21,12 +23,12 @@
         o_list_item: /^ {0,3}\d{1,9}(\.|\)) /
     };
 
-    //After passing marker test, use Capture the parse the content.
+    //After passing marker test, use Capture to the parse the content.
     var Capture = {
-        atx: /^ {0,3}(#{1,6})(.* (?=#)|.*$)/
+        atx: /^ {0,3}(#{1,6})(.* (?=#)|.*$)/,
+        u_list_item: /^ {0,3}([-_*])/,
+        o_list_item: /^ {0,3}(\d{1,9})(\.|\))/
     };
-
-    global.Capture = Capture;
 
     var helpers = {
         indentWidth: function(line){
@@ -72,6 +74,8 @@
         var line = this.src[this.idx ++];
         var indentWidth = helpers.indentWidth(line);
         if(this.isUListItemStart(line)){
+            var cap = Capture.u_list_item.exec(line);
+            this.misc.bullet = cap[1];
             this.misc.markerWidth = indentWidth + 2;
 
             var markerTrimmed = line.substring(this.misc.markerWidth);
@@ -80,7 +84,8 @@
             this.content = [markerTrimmed];
             
         }else if(this.isOListItemStart(line)){
-            var cap = /^ {0,3}(\d{1,9})(\.|\))/.exec(line);
+            var cap = Capture.o_list_item.exec(line);
+            this.misc.bullet = cap[2];
             this.misc.markerWidth = indentWidth + cap[1].length + 1;
 
             var markerTrimmed = line.substring(this.misc.markerWidth);
@@ -103,22 +108,39 @@
         while(this.idx < this.src.length){
             var line = this.src[this.idx];
             var indentWidth = helpers.indentWidth(line);
-            
-            if(this.isBlockQuote(line) && this._type === 'block_quote'){
-                this.content.push(line.substring(indentWidth + 2)); this.idx ++;
-            }else if(this.isIndentedCode(line) && this._type === 'indented_code'){
-                this.content += line + '\n'; this.idx ++;
-            }else if(!this.isUListItemStart(line) && this._type === 'u_list_item' && indentWidth >= this.misc.itemIndent){
-                this.content.push(line.substring(this.misc.markerWidth)); this.idx ++;
-            }else if(!this.isOListItemStart(line) && this._type === 'o_list_item' && indentWidth >= this.misc.itemIndent){
-                this.content.push(line.substring(this.misc.markerWidth)); this.idx ++;
+            console.log('oops');
+
+            if(this.isOListItemStart(line)){
+                this.open = false;
+            }else if(this.isUListItemStart(line)){
+                this.open = false;
+            }else if(this._type === 'u_list_item' && indentWidth >= this.misc.itemIndent){
+                this.content.push(line.substring(this.misc.markerWidth));
+                this.idx ++;
+            }else if(this._type === 'o_list_item' && indentWidth >= this.misc.itemIndent){
+                this.content.push(line.substring(this.misc.markerWidth));
+                this.idx ++;
+            }else if(this.isBlockQuote(line)){
+                if(this._type === 'block_quote') {
+                    this.content.push(line.substring(indentWidth + 2));
+                    this.idx++;
+                }else{
+                    this.open = false;
+                }
+            }else if(this.isIndentedCode(line)){
+                if(this._type === 'indented_code') {
+                    this.content += line + '\n';
+                    this.idx++;
+                }else{
+                    this.open = false;
+                }
             }else if(this._type === 'paragraph'){
-                //paragraph
-                this.content += line + '\n'; this.idx ++;
+                this.content += line + '\n';
+                this.idx ++;
             }else{
                 this.open = false;
-                break;
             }
+            if(!this.open){ break; }
         }
         this.open = false;
     };
@@ -162,7 +184,7 @@
     Block.prototype.parse = function(){
 
         if(this._isContainer) {
-            while (this.idx < this.src.length) {console.log('oops')
+            while (this.idx < this.src.length) {
                 var line = this.src[this.idx];
                 var child = null;
 
@@ -209,5 +231,58 @@
     };
 
     global.Parser = Parser;
+    global.Capture = Capture;
 
+})(this);
+
+(function(global){
+    "use strict";
+
+    /**
+     * Transfer block tree to html.
+     */
+
+    function Translator(doc){
+        this.doc = doc;
+    }
+
+    Translator.prototype.translate = function(){
+        this.doTranslate(this.doc);
+    };
+
+    Translator.prototype.doTranslate = function(block){
+
+        var tpl = this.tpl(block);
+        var html = '';
+
+        var inList = false;
+        for(var i = 0; i < block.children.length; i++){
+            var child = block.children[i];
+            
+            html += this.doTranslate(child);
+
+        }
+        html = tpl.replace('{}', html);
+        return html;
+    };
+
+    Translator.prototype.tpl = function(block){
+        var type = block.type;
+        if(type === 'thematic'){
+            return '<hr>';
+        }else if(type === 'atx'){
+            var lvl = block.misc.lvl;
+            return '<h' + lvl + '>{}</h' + lvl + '>';
+        }else if(type === 'indented_code'){
+            return '<pre><code>{}</code></pre>';
+        }else if(type === 'paragraph'){
+            return '<p>{}</p>';
+        }else if(type === 'block_quote'){
+            return '<blockquote>{}</blockquote>';
+        }else if(type === 'u_list_item' || type === 'o_list_item'){
+            return '<li>{}</li>';
+        }
+    };
+
+    global.Translator = Translator;
 })(this);
