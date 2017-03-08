82c70de4f91156a6f866643cd2220811e4819422
iam91 <zhengwy91@foxmail.com>
2017-03-08 11:59:07 +0800

    refactor

--- src/zmarkdown.js
+++ src/zmarkdown.js
@@ -4,6 +4,11 @@
 (function(global){
     "use strict";
 
+    // todo 不同列表`marker`需要建立新的列表
+    // todo 有序列表，重构列表的添加
+    // todo `blank line`不会截断`indented code`
+    // todo `blank line`造成`loose list`
+
     var Marker = {
         blank_line: /^\s*$/,
 
@@ -13,10 +18,15 @@
 
         block_quote: /^ {0,3}> /,
         u_list_item: /^ {0,3}[-_*] /,
-        o_list_item: /^ {0,3}\d{1,9}\. /
+        o_list_item: /^ {0,3}\d{1,9}(\.|\)) /
     };
 
-    //var Capture;
+    //After passing marker test, use Capture the parse the content.
+    var Capture = {
+        atx: /^ {0,3}(#{1,6})(.* (?=#)|.*$)/
+    };
+
+    global.Capture = Capture;
 
     var helpers = {
         indentWidth: function(line){
@@ -25,18 +35,105 @@
         }
     };
 
-    function Block(type, isContainer, lines){
-        this.type = type;
-        this.isContainer = isContainer;
-        this.lines = lines;
+    function Block(type, isContainer, isMulti, idx, src){
+        this._type = type;
+        this._isContainer = isContainer;
+        this._isMulti = isMulti;
+        this.src = src.slice();
+
+        this.idx = idx;
+        this.open = isMulti;
 
         this.children = [];
-
-        this.cache = [];
-        this.prevType = null;
-        this.listItemMarkerWidth = 0;
-        this.listItemIndentWidth = 0;
+        this.content = null;
+        this.misc = {};
     }
+
+    Block.prototype.consume = function(){
+        var retIdx = 0;
+        if(this._isMulti){
+            this.consumeFirst();
+            this.consumeMulti();
+            retIdx = this.idx;
+            if(this._isContainer){
+                //content needs to be parsed
+                this.src = this.content;
+                this.idx = 0;
+                this.parse(); 
+            }
+        }else{
+            this.consumeOne();
+            retIdx = this.idx;
+        }
+        return retIdx;
+    };
+
+    Block.prototype.consumeFirst = function(){
+        var line = this.src[this.idx ++];
+        var indentWidth = helpers.indentWidth(line);
+        if(this.isUListItemStart(line)){
+            this.misc.markerWidth = indentWidth + 2;
+
+            var markerTrimmed = line.substring(this.misc.markerWidth);
+            var markerTrimmedIndent = helpers.indentWidth(markerTrimmed);
+            this.misc.itemIndent = this.misc.markerWidth + markerTrimmedIndent;
+            this.content = [markerTrimmed];
+            
+        }else if(this.isOListItemStart(line)){
+            var cap = /^ {0,3}(\d{1,9})(\.|\))/.exec(line);
+            this.misc.markerWidth = indentWidth + cap[1].length + 1;
+
+            var markerTrimmed = line.substring(this.misc.markerWidth);
+            var markerTrimmedIndent = helpers.indentWidth(markerTrimmed);
+            this.misc.itemIndent = this.misc.markerWidth + markerTrimmedIndent;
+            this.content = [markerTrimmed];
+            
+        }else if(this.isIndentedCode(line)){
+            this.content = line.substring(4) + '\n';
+        }else if(this.isBlockQuote(line)){
+            this.content = [line.substring(indentWidth + 2)];
+        }else{
+            //paragraph
+            this.content = line.trim() + '\n';
+        }
+    };
+
+    Block.prototype.consumeMulti = function(){
+
+        while(this.idx < this.src.length){
+            var line = this.src[this.idx];
+            var indentWidth = helpers.indentWidth(line);
+            
+            if(this.isBlockQuote(line) && this._type === 'block_quote'){
+                this.content.push(line.substring(indentWidth + 2)); this.idx ++;
+            }else if(this.isIndentedCode(line) && this._type === 'indented_code'){
+                this.content += line + '\n'; this.idx ++;
+            }else if(!this.isUListItemStart(line) && this._type === 'u_list_item' && indentWidth >= this.misc.itemIndent){
+                this.content.push(line.substring(this.misc.markerWidth)); this.idx ++;
+            }else if(!this.isOListItemStart(line) && this._type === 'o_list_item' && indentWidth >= this.misc.itemIndent){
+                this.content.push(line.substring(this.misc.markerWidth)); this.idx ++;
+            }else if(this._type === 'paragraph'){
+                //paragraph
+                this.content += line + '\n'; this.idx ++;
+            }else{
+                this.open = false;
+                break;
+            }
+        }
+        this.open = false;
+    };
+
+    Block.prototype.consumeOne = function(){
+        var line = this.src[this.idx ++];
+        if(this._type === 'atx'){
+
+            var cap = Capture.atx.exec(line);
+            this.misc.lvl = cap[1].length;
+            this.content += cap[2].trim();
+        }else if(this._type === 'thematic'){
+            //no op
+        }
+    };
     
     Block.prototype.isAtx = function(line){
         return Marker.atx.test(line);
@@ -47,187 +144,68 @@
     };
     
     Block.prototype.isBlockQuote = function(line){
-        return Marker.thematic.test(line);
+        return Marker.block_quote.test(line);
     };
 
     Block.prototype.isIndentedCode = function(line){
-        return !this.isUListItemContent(line)
-            && !this.isOListItemContent(line)
-            && Marker.indented_code.test(line);
-    }
+        return Marker.indented_code.test(line);
+    };
 
     Block.prototype.isUListItemStart = function(line){
         return Marker.u_list_item.test(line);
     };
 
-    Block.prototype.isUListItemContent = function(line){
-        var indentWidth = helpers.indentWidth(line);
-        var _prevIsList = this.prevType === 'u_list_start' || this.prevType === 'u_list_content';
-        return _prevIsList && indentWidth >= this.listItemIndentWidth;
-    };
-
     Block.prototype.isOListItemStart = function(line){
         return Marker.o_list_item.test(line);
     };
-
-    Block.prototype.isOListItemContent = function(line){
-        var indentWidth = helpers.indentWidth(line);
-        var _prevIsList = this.prevType === 'o_list_start' || this.prevType === 'o_list_content';
-        return _prevIsList && indentWidth >= this.listItemIndentWidth;
-    };
-
-
+    
     Block.prototype.parse = function(){
-        var lines = this.lines;
-        if(this.isContainer) {
-            while (lines.length) {
-                var line = lines.shift();
+
+        if(this._isContainer) {
+            while (this.idx < this.src.length) {console.log('oops')
+                var line = this.src[this.idx];
+                var child = null;
 
                 if (this.isAtx(line)) {
-                    this.oneLineBlock('atx', line);
+                    child = new Block('atx', false, false, this.idx, this.src);
                 } else if (this.isThematic(line)) {
-                    this.oneLineBlock('thematic', line);
+                    child = new Block('thematic', false, false, this.idx, this.src);
                 } else if (this.isBlockQuote(line)) {
-                    this.multiLineBlock('block_quote', line);
+                    child = new Block('block_quote', true, true, this.idx, this.src);
                 } else if (this.isUListItemStart(line)) {
-                    this.multiLineBlock('u_list_start', line);
-                } else if (this.isUListItemContent(line)) {
-                    this.multiLineBlock('u_list_content', line);
+                    child = new Block('u_list_item', true, true, this.idx, this.src);
                 } else if (this.isOListItemStart(line)){
-                    this.multiLineBlock('o_list_start', line);
-                } else if (this.isOListItemContent(line)){
-                    this.multiLineBlock('o_list_content', line);
+                    child = new Block('o_list_item', true, true, this.idx, this.src);
                 } else if (this.isIndentedCode(line)) {
-                    this.multiLineBlock('indented_code', line);
+                    child = new Block('indented_code', false, true, this.idx, this.src);
                 } else {
                     //paragraph
-                    this.multiLineBlock('paragraph', line);
+                    child = new Block('paragraph', false, true, this.idx, this.src);
                 }
+                this.idx = child.consume();
+                child.parse();
+                this.children.push(child);
             }
-            this.closePrev();
-            return this.children.slice();
-        }else{
-            //parse inline
-            return null;
         }
     };
-    
-    Block.prototype.closePrev = function(){
-        if(this.prevType){
-            var _cache = [];
-            var _type = this.prevType;
-            while(this.cache.length){
-                _cache.push(this.cache.shift());
-            }
-
-            _type = _type === 'u_list_start' || _type === 'u_list_content'
-                ? 'u_list_item' : _type;
-
-            _type = _type === 'o_list_start' || _type === 'o_list_content'
-                ? 'o_list_item' : _type;
-
-            var container = _type === 'block_quote'
-                || _type === 'u_list_item'
-                || _type === 'o_list_item';
-
-            var prev = new Block(_type, container, _cache);
-
-            this.prevType = null;
-            this.listItemMarkerWidth = 0;
-            this.listItemIndentWidth = 0;
-            this.children.push(prev);
-        }
-    };
-    
-    Block.prototype.multiLineBlock = function(type, line){
-
-        if(!this.prevType){
-            this.prevType = type;
-        }else if(type === 'u_list_start' || type === 'o_list_start'){
-            this.closePrev();
-        }else if(type !== this.prevType
-            && !(type === 'u_list_content' && this.prevType === 'u_list_start')
-            && !(type === 'o_list_content' && this.prevType === 'o_list_start')){
-            this.closePrev();
-        }
-
-        var indentWidth = helpers.indentWidth(line);
-        
-        if(type === 'block_quote'){
-            this.cache.push(line.substring(indentWidth + 2));
-            this.prevType = 'block_quote';
-        }else if(type === 'u_list_start'){
-            this.listItemMarkerWidth = indentWidth + 2;
-
-            var markerTrimmed = line.substring(this.listItemMarkerWidth);
-            var markerTrimmedIndent = helpers.indentWidth(markerTrimmed);
-            this.listItemIndentWidth = this.listItemMarkerWidth + markerTrimmedIndent;
-
-            this.cache.push(markerTrimmed);
-            this.prevType = 'u_list_start';
-        }else if(type === 'u_list_content'){
-            this.cache.push(line.substring(this.listItemMarkerWidth));
-            this.prevType = 'u_list_content';
-        }else if(type === 'o_list_start'){
-            var cap = /^ {0,3}(\d{1,9}\.)/.exec(line);
-            this.listItemMarkerWidth = indentWidth + cap[1].length;
-
-            var markerTrimmed = line.substring(this.listItemMarkerWidth);
-            var markerTrimmedIndent = helpers.indentWidth(markerTrimmed);
-            this.listItemIndentWidth = this.listItemMarkerWidth + markerTrimmedIndent;
-
-            this.cache.push(markerTrimmed);
-            this.prevType = 'o_list_start';
-        }else if(type === 'o_list_content'){
-            this.cache.push(line.substring(this.listItemMarkerWidth));
-            this.prevType = 'o_list_content';
-        }else if(type === 'indented_code'){
-            this.cache.push(line.substring(4));
-            this.prevType = 'indented_code';
-        }else{
-            //paragraph
-            this.cache.push(line);
-            this.prevType = 'paragraph';
-        }
-    };
-
-    Block.prototype.oneLineBlock = function(type, line){
-
-        this.closePrev();
-
-        var child = null;
-        if(type === 'atx'){
-            var cap = /^ {0,3}#{1,6}($| +)/.exec(line);
-            child = new Block('atx', false, ['']);
-        }else if(type === 'thematic'){
-            child = new Block('thematic', false, ['']);
-        }
-        this.children.push(child);
-    };
-
 
     function Parser(){
-        this.lines = null;
         this.doc = null;
     }
 
-    Parser.prototype.parse = function(src){
+    Parser.prototype.preProc = function(src){
         //pre-process
         src = src
             .replace(/\r\n?|\n/g, '\n')
             .replace(/\t/g, '    ');
-        this.lines = src.split(/\n/);
+        return src.split(/\n/);
+    };
 
-        var queue = [];
-        var doc = new Block('doc', true, this.lines.slice());
+    Parser.prototype.parse = function(src){
+        src = this.preProc(src);
+        var doc = new Block('doc', true, true, 0, src);
+        doc.parse();
         this.doc = doc;
-        queue.push(doc);
-
-        while(queue.length){
-            var block = queue.shift();
-            var childBlocks = block.parse() || [];
-            queue = queue.concat(childBlocks);
-        }
     };
 
     global.Parser = Parser;
